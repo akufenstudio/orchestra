0cb284471e8dd494ad85bec74993d3f71b3dd0a5
Nicholas Charbonneau <nicholas.charbonneau@gmail.com>
2016-06-27 10:28:20 -0400

    v0.1.0

--- CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-# CHANGELOG
-
-## 0.1.x
-
-0.1.0 (2016-06-14)
-    - Initial alpha testing release
--- tests/assets/app/Controllers/IndexController.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace App\Controllers;
-
-class IndexController extends ControllerBase
-{
-}
