8d91f06a6d71e11f362b693a29d52d14b00c090c
Nicholas Charbonneau <nicholas.charbonneau@gmail.com>
2016-07-25 15:14:09 -0400

    Improved MySQL support for Travis.

--- tests/assets/wp-config.php
+++ tests/assets/wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'orchestra');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
